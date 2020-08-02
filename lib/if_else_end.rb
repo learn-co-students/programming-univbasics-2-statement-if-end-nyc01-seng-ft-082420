ff22488d2a2f471366720be40c5320985462f7aa
groana <contact@groanamelendez.com>
2020-08-02 16:34:50 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,10 @@
 # Write your solution here
+
+# current_time = Time.now
+# current_time = current_time.to_i #why do we have to convert it to an integer? it's not an integer already?
+
+if (Time.now.to_i.even?)
+  puts "Even!"
+else
+  puts "Odd!"
+end
